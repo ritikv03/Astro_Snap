8451163457def03b8bb212a37b9cb8aa3dd98883
Ritik Verma <ritikverma@icloud.com>
2024-06-23 11:46:42 +0530

    added framer motion to hero section

--- client/src/components/Content.jsx
+++ client/src/components/Content.jsx
@@ -1,18 +1,33 @@
 import React from 'react'
+import { motion } from 'framer-motion';
 
 const Content = () => {
   return (
     <div>
       <div className="absolute top-0 left-0 h-full w-full bg-black opacity-50"></div>
       <div className="absolute top-0 left-0 h-full w-full flex flex-col items-center justify-center">
-        <h1 className="text-white text-4xl md:text-6xl font-bold mb-20">AstroSnap</h1>
+        <motion.h1
+          initial = {{ opacity: 0, y : -100}}
+          animate = {{ opacity: 1 , y : 0}}
+          transition={{ duration: 1 }} 
+          className="text-white text-4xl md:text-6xl font-bold mb-20">
+          AstroSnap
+        </motion.h1>
         <div className="flex space-x-4">
-          <button className="bg-purple-800 hover:bg-purple-900 text-white font-bold py-2 px-4 rounded shadow-md">
+          <motion.button 
+            initial = {{ opacity : 0, x : -100}}
+            animate = {{ opacity : 1, x : 0}}
+            transition={{ duration : 1.5}}
+            className="bg-purple-800 hover:bg-purple-900 text-white font-bold py-2 px-4 rounded shadow-md">
             Get Image
-          </button>
-          <button className="bg-purple-800 hover:bg-purple-900 text-white font-bold py-2 px-4 rounded shadow-md">
+          </motion.button>
+          <motion.button
+            initial = {{ opacity : 0, x : 100}}
+            animate = {{ opacity : 1, x : 0}}
+            transition={{ duration : 1.5}} 
+            className="bg-purple-800 hover:bg-purple-900 text-white font-bold py-2 px-4 rounded shadow-md">
             Enter Data
-          </button>
+          </motion.button>
         </div>
       </div>
     </div>
